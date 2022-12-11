834f2a4958c22095bbfb2a521cb04910453f9e3e
vasanth5627 <44662233+vasanth5627@users.noreply.github.com>
2022-12-12 01:24:11 +0530

    Update script.js

--- AdvancedJs/11122022/one/script.js
+++ AdvancedJs/11122022/one/script.js
@@ -1,43 +1,77 @@
 
-function setError(id,error){
-    const element = document.getElementById(id);
-    element.getElementsByClassName('formerror')[0].innerHTML = error;
-}
-
-function clearErrors(){
-    const errors = document.getElementsByClassName('formerror');
-    for(let item of errors){
-        item.innerHTML="";
-    }
-}
-
-
-function validateForm(){
-    var retval = true;
-    clearErrors()
-    var name = document.forms['myform']["fname"].value;
-    if(name.length<5){
-        retval =false;
-        setError("name","*lenght is too short");
-    }
-   
-    var phone = document.forms['myform']["fphone"].value;
-    if(phone.length!=10){
-        retval =false;
-        setError("phone","*lenght should be 10");
-    }
-
-    var pass = document.forms["myform"]["cpass"].value;
-    if(pass.length<8){
-        retval =false;
-        setError("pass","*Passwod too small");
-    }
-
-     var cpass = document.forms["myform"]["fcpass"].value;
-        if(cpass!=pass){
-            retval =false;  
-            setError("cpass","*Passwod donot match");
+    function onFormSubmit(retval){
+        if(retval){
+            var formData = readFormData();
+            insertNewData(formData);
+            clearFormData();
         }
-    return retval;
-}
+    
+
+    }
+    function clearFormData(){
+        const form = document.forms['myform'].children;
+        for(let i=0;i<form.length-1;i++){
+            console.log(form[i].getElementsByTagName('input'))
+        }
+    }
+
+    function readFormData(){
+        var formData = {};
+        formData["name"] = document.getElementById('name');
+        formData["email"] = document.getElementById('email');
+        formData["setpassword"] = document.getElementById('pass');
+        formData["cpass"] = document.getElementById('cpass');
+        return formData;
+        
+    }
+
+    function insertNewData(data){
+        var table = document.getElementById('employee-list').getElementsByTagName('tbody')[0];
+        var newRow = table.insertRow(table.length);
+        cell1 = newRow.insertCell(0);
+        cell1.innerHTML = data.name.value;
+        cell2 = newRow.insertCell(1);
+        cell2.innerHTML = data.email.value;
+
+    }
+
+    function setError(id,error){
+        const element = document.getElementById(id);
+        console.log(element);
+        element.getElementsByClassName('formerror')[0].innerHTML = error;
+    }
+
+    function clearErrors(){
+        const errors = document.getElementsByClassName('formerror');
+        for(let item of errors){
+            item.innerHTML="";
+        }
+    }
+
+
+    function validateForm(){
+        var retval = true;
+        clearErrors()
+        var name = document.forms['myform']["name"].value;
+        if(name.length<5){
+            
+            retval =false;
+            setError("name1","*length is too short");
+        }
+    
+        
+
+        var pass = document.forms["myform"]["password"].value;
+        if(pass.length<8){
+            retval =false;
+            setError("passid","*Passwod too small");
+        }
+
+        var cpass = document.forms["myform"]["cpass"].value;
+            if(cpass!=pass){
+                retval =false;  
+                setError("cpassid","*Passwod don't match");
+            }
+        return onFormSubmit(retval);
+    }
 
